0089b9895b5edccb448368c4584ab009c96359ce
igorifsp <bandeira.igor@aluno.ifsp.edu.br>
2024-03-05 15:21:50 -0300

    eye to password

--- front/cadastro/script.js
+++ front/cadastro/script.js
@@ -37,3 +37,24 @@ document
       console.error("Erro ao enviar dados para a API:", error.message);
     }
   });
+
+function togglePasswordVisibility() {
+  var passwordField = document.getElementById("password");
+  var toggleIcon = document.getElementById("togglePassword");
+
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    toggleIcon.classList.remove("fa-eye-slash");
+    toggleIcon.classList.add("fa-eye");
+  } else {
+    passwordField.type = "password";
+    toggleIcon.classList.remove("fa-eye");
+    toggleIcon.classList.add("fa-eye-slash");
+  }
+}
+
+document
+  .getElementById("togglePassword")
+  .addEventListener("click", function () {
+    togglePasswordVisibility();
+  });
